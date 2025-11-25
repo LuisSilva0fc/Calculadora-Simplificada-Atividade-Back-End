6b51fd2bc10fe72da83f5cee71d47e49e3156c0a
LuisSilva0fc <nandosilvaabentp84@gmail.com>
2025-11-25 13:49:18 -0300

    Ajustes de notas

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// ISSO AQUI SUBSTITUI O "GET" DA PÁGINA INICIAL
 // Ele serve o index.html, o style.css e o script.js automaticamente
 app.use(express.static('public')); 
 
